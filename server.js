7954c8c767a8dd52cb6552ffd298350ef69103ac
Mateusz Lencki <mateusz.lencki@gmail.com>
2019-11-27 21:25:32 +0100

    Init

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const http = require('http');
+const httpProxy = require('http-proxy');
+const services = require('./serviceRegistry').services;
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer(function(request, response) {
+
+    let requestedPrefix = request.url.split('/')[1];
+    let targetPath = request.url.split(requestedPrefix)[1];
+
+    let selectedService = services.find(function (service) {
+        return service.prefix === requestedPrefix
+    });
+
+    if (!selectedService) {
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not found");
+        response.end();
+        return;
+    }
+
+    request.url = targetPath;
+
+    proxy.web(request, response, {
+        target: selectedService.target,
+        changeOrigin: true,
+    });
+});
+
+server.listen(PORT);
+
+console.log(`API Gateway is listening on port: ${PORT}`);
